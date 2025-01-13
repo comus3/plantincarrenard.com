2c31da5f533386853e84de8b735f50291a855a2f
comus3 <come.plantin.carrenard@gmail.com>
2025-01-13 21:48:38 +0000

    Update startTime in script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const startTime = new Date("2025-01-13T21:30:16Z");
+const startTime = new Date("2025-01-13T21:48:38Z");
 const textElement = document.getElementById("animated-text");
 
 // Final stages of the text
