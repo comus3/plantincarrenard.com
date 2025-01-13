9d23d471bb477a2d15e779a0587aa8c9b9b1ceab
comus3 <come.plantin.carrenard@gmail.com>
2025-01-13 22:22:24 +0100

    Remove unused posts.json.js file and clean up posts.json structure

--- posts.json.js
+++
@@ -1,3 +0,0 @@
-const posts = [
-  { title: "first_post", url: "blog_posts/first_post.html", last_modified: "2025-01-13", created: "1736803139" },
-  { title: "test_page", url: "blog_posts/test_page.html", last_modified: "2025-01-13", created: "1736803139" },
