902ab92ef9a9cea09727129ccb6bcaa21c8d479e
comus3 <come.plantin.carrenard@gmail.com>
2025-01-13 20:52:12 +0000

    Update blog posts list

--- blog_script.js
+++ blog_script.js
@@ -1,4 +1,3 @@
 const posts = [
-  { title: "first_post", url: "blog_posts/first_post.html", last_modified: "2025-01-13", created: "1736801181" },
-  { title: "test_page", url: "blog_posts/test_page.html", last_modified: "2025-01-13", created: "1736801181" },
-];
+  { title: "first_post", url: "blog_posts/first_post.html", last_modified: "2025-01-13", created: "1736801531" },
+  { title: "test_page", url: "blog_posts/test_page.html", last_modified: "2025-01-13", created: "1736801531" },
